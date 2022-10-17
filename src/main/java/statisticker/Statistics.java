c5f527cc59eabe46efe1b9f5735febd12ea08ce6
Kesavan-E <116025852+Kesavan-E@users.noreply.github.com>
2022-10-17 23:45:54 +0530

    Update Statistics.java
    Declared the variables with proper values and methods

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -4,7 +4,28 @@ import java.util.List;
 
 public class Statistics 
 {
-    public static Stats getStatistics(List<___> numbers) {
+    static class Stats
+    {
+        public Float min = Float.NaN;
+        public Float max = Float.NaN;
+        public Float average = Float.NaN;
+    }
+    
+    public static Stats getStatistics(List<Float> numbers) {
         //implement the computation of statistics here
+        Statistics.Stats s = new Stats();
+        if(numbers.isEmpty())
+        {
+            return(s);
+        }
+        Float sum = 0f;
+        s.min=Collections.min(numbers);
+        s.max=Collections.max(numbers);
+        for(Float n:numbers)
+        {
+            sum=+n;
+        }    
+        s.average = sum/(numbers.size());
+        return(s);
     }
 }
